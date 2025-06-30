4fd8028fb602079aaf93677dc5b27aa3253c61d1
igoramorim <igor340@gmail.com>
2025-06-29 21:08:43 -0300

    scripts to copy files

---
+++ copy-to-machine.sh
@@ -0,0 +1,5 @@
+cp ./.tmux.conf ~/.tmux.conf 
+cp ./.zshrc ~/.zshrc
+cp -R ./.config/ghostty ~/.config/
+cp -R ./.config/nvim ~/.config/
+cp ./idea/.ideavimrc ~/.ideavimrc 
---
+++ copy-to-repo.sh
@@ -0,0 +1,5 @@
+cp ~/.tmux.conf ./.tmux.conf
+cp ~/.zshrc ./.zshrc
+cp -R ~/.config/ghostty ./.config/
+cp -R ~/.config/nvim ./.config/
+cp ~/.ideavimrc ./idea/.ideavimrc
